223f87323e1c8af3109443def65009d0d818a77e
Ayeshakhan14 <ayeshatravels9@gmail.com>
2023-11-07 20:15:23 +0530

    api omdb

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 const express=require("express")
-const mongoose=require("mongoose")
 const app=express()
 const cors = require('cors');
-const MovieRouter = require("./route/movie.route");
 app.use(express.json())
 app.use(cors())
+const axios = require("axios")
+
+const port=8000
+
+app.get('/movies', async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+
+    const response = await axios.get(`https://www.omdbapi.com/?s=war&type=movie&page=1&apikey=1daaa09d`);
+    
+    const movie=  res.json(response.data.Search)
+    console.log(movie)
+    res.send(movie)
+      
+
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching movies.' });
+  }
+});
+
+app.get('/movies/:id', async (req, res) => {
+  const movieId = req.params.id;
+
+  try {
+    const response = await axios.get(`https://www.omdbapi.com/?i=${movieId}&apikey=1daaa09d`);
+    const movie = response.data;
+    res.send(movie);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching the movie by ID.' });
+  }
+});
 
 
 
-app.use("/",MovieRouter)
 
 
 
-const PORT = 8000;
-const MONGO_URI =
-  "mongodb+srv://ayeshatravels9:movie@cluster0.ecnhyms.mongodb.net/Movie_db";
-
-
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-
- 
-  
-
-  app.use((req, res) => {
-      return res.status(400).send({ status: false, message: "End point is incorrect" })
-  });
-
-
-  
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => {
+  console.log(`Proxy API server is running on port ${port}`);
+});
 
 
